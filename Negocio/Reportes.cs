58eba70e0e0efdf4b068cf9aaaaadecd0f2314a7
mitzihdz <mitzi.hernandez@people-media.com.mx>
2022-06-30 17:06:44 -0500

    Reporte estatus ruta

---
+++ Negocio/Reportes.cs
@@ -0,0 +1,35 @@
+﻿using AccesoDatos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Negocio
+{
+    public class Reportes
+    {
+        private transportesContext ctx = new transportesContext();
+        public Response Response = new Response();
+
+        public Response ReporteEstatusRutas()
+        {
+            try
+            {
+                List<TblSolicitudDetalle> list = ctx.TblSolicitudDetalles
+                    .Include(s => s.TblSolicitud).ThenInclude(c => c.TblClientes)
+                    .Include(d => d.TblCajas)
+                    .Include(d => d.TblOperador).Include(d => d.TblTracto).Include(d => d.TblEstatusRuta)
+                    //.Include(r => r.TblSolicitudDetalleRuta)
+                    .Where(x => x.TblSolicitud.TblEstatusId != 7).ToList();
+
+                Response.Estado = true;
+                Response.Mensaje = "OK";
+                Response.Respuesta = list;
+            }
+            catch (Exception ex)
+            {
+                Response.Estado = false;
+                Response.Mensaje = ex.Message;
+            }
+
+            return Response;
+        }
+    }
+}
---
+++ Servicios/Controllers/ReporteController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AccesoDatos.Models;
+using Negocio;
+
+namespace Servicios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReporteController : ControllerBase
+    {
+        private Reportes reportes = new Reportes();
+
+        [HttpGet]
+        [Route("ReporteEstatusRutas")]
+        public Response ReporteEstatusRutas()
+        {
+            return reportes.ReporteEstatusRutas();
+        }
+    }
+}
